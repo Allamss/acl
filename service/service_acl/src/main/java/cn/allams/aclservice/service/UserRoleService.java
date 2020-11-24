767bd6dba342b95f5921470b103f09718ed0f15e
allamss <allamss@gmail.com>
2020-11-24 23:13:55 +0800

    [feat]acl服务

---
+++ service/service_acl/src/main/java/cn/allams/aclservice/service/UserRoleService.java
@@ -0,0 +1,17 @@
+package cn.allams.aclservice.service;
+
+
+import cn.allams.aclservice.entity.UserRole;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-01-12
+ */
+public interface UserRoleService extends IService<UserRole> {
+
+}
---
+++ service/service_acl/src/main/java/cn/allams/aclservice/service/impl/UserRoleServiceImpl.java
@@ -0,0 +1,20 @@
+package cn.allams.aclservice.service.impl;
+
+import cn.allams.aclservice.entity.UserRole;
+import cn.allams.aclservice.mapper.UserRoleMapper;
+import cn.allams.aclservice.service.UserRoleService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-01-12
+ */
+@Service
+public class UserRoleServiceImpl extends ServiceImpl<UserRoleMapper, UserRole> implements UserRoleService {
+
+}
